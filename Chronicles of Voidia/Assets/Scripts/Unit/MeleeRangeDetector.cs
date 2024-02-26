cc491b11c07d2546ee15daadd10d7a5347d41496
Arthur <35684550+Daedras10@users.noreply.github.com>
2024-02-26 14:23:56 +0100

    Squeleton prefab + melee range detector

---
+++ Chronicles of Voidia/Assets/Scripts/Unit/MeleeRangeDetector.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace Unit
+{
+    public class MeleeRangeDetector : MonoBehaviour
+    {
+        public event Action<Collider> OnMeleeRangeEnter; // Another type of object ?
+        public event Action<Collider> OnMeleeRangeExit;
+        
+        private void OnTriggerEnter(Collider other)
+        {
+            OnMeleeRangeEnter?.Invoke(other);
+        }
+        
+        private void OnTriggerExit(Collider other)
+        {
+            OnMeleeRangeExit?.Invoke(other);
+        }
+    }
+}
